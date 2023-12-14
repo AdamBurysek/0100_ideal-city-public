45eb0ce8e4b449e401502b7745618c09a1a0c9e6
AdamBurysek <114564710+AdamBurysek@users.noreply.github.com>
2023-12-14 14:17:37 +0100

    api add

--- src/renderer/src/apiAxios.ts
+++ src/renderer/src/apiAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://eu-central-1.aws.data.mongodb-api.com/app/vida-4k-camera-ttzph/endpoint",
+    "https://eu-central-1.aws.data.mongodb-api.com/app/idealcitypublic-uwxvn/endpoint",
   headers: {
     "Content-Type": "application/json",
   },
