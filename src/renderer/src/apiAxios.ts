289a7ef557fca82acb75f66b3099089d33711440
AdamBurysek <114564710+AdamBurysek@users.noreply.github.com>
2023-12-14 14:31:47 +0100

    api change

--- src/renderer/src/apiAxios.ts
+++ src/renderer/src/apiAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://eu-central-1.aws.data.mongodb-api.com/app/vida-serverless-breei/endpoint",
+    "https://eu-central-1.aws.data.mongodb-api.com/app/vida-4k-camera-ttzph/endpoint",
   headers: {
     "Content-Type": "application/json",
   },
